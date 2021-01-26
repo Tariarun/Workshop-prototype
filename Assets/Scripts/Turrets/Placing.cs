90cea32d9d4186b02d93edf0f8f6c65fe1b7cc61
Tariarun <zailvaix12@gmail.com>
2021-01-26 16:40:49 +0100

    Placement 02

---
+++ Assets/Scripts/Turrets/Placing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placing : MonoBehaviour
+{
+    public bool occupied = false;
+    public GameObject Turret;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = -Vector3.one;
+            clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 0f)); 
+
+            GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+            Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
+            transform.position = gridLayout.CellToWorld(cellPosition);
+
+            Instantiate(Turret, cellPosition, Quaternion.identity);
+        }
+    }
+
+    //GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+    //Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
+    //transform.position = gridLayout.CellToWorld(cellPosition);
+}
